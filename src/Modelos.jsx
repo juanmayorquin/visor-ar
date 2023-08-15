ed00d2b72dea4017ad3c71255062e5d2815a2a46
juanmayorquin <127550805+juanmayorquin@users.noreply.github.com>
2023-08-15 18:33:56 -0500

    Se añadió la imagen para el banner de la sección Modelos

--- src/Modelos.jsx
+++ src/Modelos.jsx
@@ -1,11 +1,21 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { ColorBar } from "./components/ColorBar";
+import { Banner } from "./components/Banner";
+import { Section } from "./components/Section";
+
 export function Modelos() {
     return (
         <>
             <ColorBar />
             <Header />
+            <Banner 
+                titulo={"Modelos 3D"}
+            />
+            <Section
+                texto={"¡Disfruta de esta emocionante aventura en el mundo de los modelos 3D de instrumentos biomédicos!"}
+                isBold
+            />
             <Footer/>
         </>
     );
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,20 @@
-export function Banner(){
-    return(
-        <div className="banner">
-            <div className="banner__contenido">
-                <h1 className="banner__title">Instrumentos Biomédicos</h1>
+export function Banner({titulo, tipo}){
+    if (tipo==1) {
+        return(
+            <div className="banner">
+                <div className="banner__contenido">
+                    <h1 className="banner__title">{titulo}</h1>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    else{
+        return(
+            <div className="banner--alt">
+                <div className="banner__contenido">
+                    <h1 className="banner__title">{titulo}</h1>
+                </div>
+            </div>
+        );
+    }
 }
