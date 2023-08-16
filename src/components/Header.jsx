22d7ea7e481cf93b02c94d971004abc7df70e813
Juan José Mayorquín <juanjmayor@outlook.es>
2023-08-16 17:56:44 -0500

    Se finaliza la barra de navegación sticky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
-export function Header() {
+import { useEffect } from "react";
+
+export function Header() {    
+    useEffect(() => {
+        const navbarToggle = document.getElementById('navbar-button');
+        if (navbarToggle != null) {
+            navbarToggle.addEventListener('click', () => {
+                const navbarItems = document.querySelector('.navbar__items--mobile');
+                navbarItems.classList.toggle('active');
+                navbarToggle.classList.toggle('active');
+            });
+        }
+    }, []);
     return (
-        <>
+        <div className="headerDiv">
             <header className="header">
                 <nav className="navbar">
-                    <button id="navbar-button" className="navbar__button active">
+                    <button id="navbar-button" className="navbar__button">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#D20B0B" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M4 6l16 0" />
@@ -20,6 +32,11 @@ export function Header() {
                 </nav>
                     <img className="header__logo50" src="../img/Logo-50.png" alt="Logo 50 Años" />       
             </header>
-        </>
+            <ul className="navbar__items--mobile">
+                <a href="/" className="navbar__item--mobile"><li>Inicio</li></a>    
+                <a href="/instrumentos" className="navbar__item--mobile"><li>Instrumentos</li></a>
+                <a href="/modelos" className="navbar__item--mobile"><li>Modelos</li></a>
+            </ul>
+        </div>
     );
     }
