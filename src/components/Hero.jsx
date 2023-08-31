571cb838ee314a03a0c23b066e764d5c536fa63b
Juan José Mayorquín <juanjmayor@outlook.es>
2023-08-31 16:25:56 -0500

    Se corrigieron errores menores

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export function Hero() {
     return (
         <>
-            <div className="hero container">
+            <div className="hero container-home">
                 <div className="hero__content">
                     <h1 className="hero__title"> BioAR Lab: <br /> <span>Visor de Dispositivos de Laboratorio en RA</span></h1>
                 </div>
